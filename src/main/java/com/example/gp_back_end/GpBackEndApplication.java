60634571930b5073d55c0f4f28f4013d2c3ba072
wanigarathnekaa <kavisha.abhishake@gmail.com>
2024-06-16 17:04:47 +0530

    Initial Commit

---
+++ src/main/java/com/example/gp_back_end/GpBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.gp_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GpBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gp_back_end/GpBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gp_back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GpBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
