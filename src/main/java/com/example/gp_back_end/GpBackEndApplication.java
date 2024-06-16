58998a3a3c987f8b3c04f8b8e62efb150985b406
wanigarathnekaa <kavisha.abhishake@gmail.com>
2024-06-16 17:09:49 +0530

    Welcome Message

--- src/main/java/com/example/gp_back_end/GpBackEndApplication.java
+++ src/main/java/com/example/gp_back_end/GpBackEndApplication.java
@@ -2,11 +2,20 @@ package com.example.gp_back_end;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class GpBackEndApplication {
 
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "Welcome to Coding! /n Let's create the Back End Application";
+    }
+
     public static void main(String[] args) {
+
         SpringApplication.run(GpBackEndApplication.class, args);
     }
 
