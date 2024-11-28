3f8e99fa7c9f98eb884f034c113618d0f8dbb846
Naveesha Lakshan <lakshannaveesha10@gmail.com>
2024-11-28 23:37:59 +0530

    Create courses from bulk upload

--- src/main/java/com/example/gp_back_end/config/SecurityConfiguration.java
+++ src/main/java/com/example/gp_back_end/config/SecurityConfiguration.java
@@ -28,12 +28,14 @@ public class SecurityConfiguration {
                         .requestMatchers("/api/v1/auth/**").permitAll()  // Adjust the request matchers as needed
                         .requestMatchers("/forms/**").permitAll()
                         .requestMatchers("/registration/**").permitAll()
-                        .requestMatchers("/api/v1/student/{id}/**").permitAll()
+                        .requestMatchers("/course/import").permitAll()
+//                        .requestMatchers("/api/v1/student/{id}/**").permitAll()
                         .requestMatchers("/api/v1/admin/**").hasRole(ADMIN.name())
-                        .requestMatchers("/api/v1/student/**").hasAnyRole(STUDENT.name(), ADMIN.name())
+                        .requestMatchers("/api/v1/student/**").permitAll()
                         .requestMatchers("/api/v1/lecturer/**").hasAnyRole(LECTURER.name(), ADMIN.name())
                         .requestMatchers("/send-email").permitAll()
                         .requestMatchers("/notifications").permitAll()
+
 
 //                        .anyRequest().authenticated()
                 )
