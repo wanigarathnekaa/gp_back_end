4780e25a8a12520753623541a5fa5cc4fd7510c1
NethmiHasi <nethuhasi2001@gmail.com>
2024-12-02 14:38:10 +0700

    update cloak repository

--- src/main/java/com/example/gp_back_end/repository/CloakRepository.java
+++ src/main/java/com/example/gp_back_end/repository/CloakRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.Optional;
 
 public interface CloakRepository extends MongoRepository<CloakModel, String> {
-    Optional<CloakModel> findById (String id);
+    Optional<CloakModel> findByName (String name);
 }
